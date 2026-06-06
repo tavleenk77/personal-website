9571cc3375af67a62a809733f622d2f2e740deeb
Tavleen Kaur <tavleenk77@gmail.com>
2026-06-05 20:43:13 -0700

    Remove old duplicate project

--- personal-website-builder/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-
-// Basic Vite configuration that enables React (JSX) support.
-export default defineConfig({
-  plugins: [react()],
-})
